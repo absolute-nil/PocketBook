b881856b26d5a881372c7554d45bd9f8113f5438
Vishal Salgond <vnsalgond@gmail.com>
2020-11-28 07:18:29 +0530

    Added Screens

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProflieScreen extends StatefulWidget {
+  static const String id = 'profile_screen';
+
+  @override
+  _ProflieScreenState createState() => _ProflieScreenState();
+}
+
+class _ProflieScreenState extends State<ProflieScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
