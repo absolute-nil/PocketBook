2f55433033d9b568b02ec661451d46b7b4388f33
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-11-28 21:49:28 +0530

    add subject model

---
+++ lib/models/subject.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Subject {
+  final String id;
+  final String year;
+  final String title;
+
+  Subject({@required this.id, @required this.year, @required this.title});
+}
