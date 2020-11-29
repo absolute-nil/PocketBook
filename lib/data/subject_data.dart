c4ee5ac87894a11a1d280da021cb2b06f6e77a32
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-11-29 23:22:42 +0530

    add subject data

---
+++ lib/data/subject_data.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:pocketbook/model/subject.dart';
+
+const SUBJECT_DATA = const [
+  Subject(id: 's1', year: 'c1', title: 'Applied Mathematics'),
+  Subject(id: 's2', year: 'c1', title: 'Engineering Chemistry'),
+  Subject(id: 's3', year: 'c1', title: 'Engineering Physics'),
+  Subject(id: 's4', year: 'c1', title: 'Engineering Drawing'),
+  Subject(id: 's5', year: 'c1', title: 'Programming in C'),
+  Subject(
+      id: 's6',
+      year: 'c1',
+      title: 'Elements of Electrical & Electronics Engineering'),
+  Subject(id: 's7', year: 'c1', title: 'Communication Skills'),
+  Subject(id: 's8', year: 'c1', title: 'Environmental Studies'),
+  Subject(id: 's9', year: 'c1', title: 'Engineering Mechanics'),
+  Subject(id: 's10', year: 'c2', title: 'Discrete and Applied Mathematics'),
+  Subject(id: 's11', year: 'c2', title: 'Data Structures'),
+  Subject(id: 's12', year: 'c2', title: 'Database Management Systems'),
+  Subject(id: 's13', year: 'c2', title: 'Digital Systems'),
+  Subject(id: 's14', year: 'c2', title: 'Data Communication and Networking'),
+  Subject(id: 's15', year: 'c2', title: 'Java'),
+  Subject(id: 's16', year: 'c2', title: 'Python'),
+  Subject(
+      id: 's17',
+      year: 'c2',
+      title: 'Probability, Statistics and Optimization Techniques'),
+  Subject(id: 's18', year: 'c2', title: 'Information Theory and Coding'),
+  Subject(id: 's19', year: 'c2', title: 'Analysis of Algorithms'),
+  Subject(id: 's20', year: 'c2', title: 'Advanced Databases'),
+  Subject(id: 's21', year: 'c2', title: 'Web Programming'),
+  Subject(id: 's22', year: 'c2', title: 'Advanced Java'),
+  Subject(id: 's23', year: 'c2', title: 'Advanced Python'),
+  Subject(id: 's24', year: 'c3', title: 'Theory of Computation'),
+  Subject(id: 's25', year: 'c3', title: 'Operating System'),
+  Subject(id: 's26', year: 'c3', title: 'Information and Network Security'),
+  Subject(id: 's27', year: 'c3', title: 'UI Programming'),
+  Subject(id: 's28', year: 'c3', title: 'Artificial Intelligence'),
+  Subject(id: 's29', year: 'c3', title: 'Flutter'),
+  Subject(id: 's30', year: 'c3', title: 'PHP'),
+  Subject(
+      id: 's31', year: 'c3', title: 'Object Oriented Software Engineering '),
+  Subject(id: 's32', year: 'c3', title: 'Simulation and Modeling'),
+  Subject(id: 's33', year: 'c3', title: 'Cloud Computing'),
+  Subject(id: 's34', year: 'c3', title: 'Data Analysis'),
+  Subject(id: 's35', year: 'c3', title: 'Development Frameworks'),
+  Subject(
+      id: 's36', year: 'c4', title: 'Software Testing and Quality Assurance'),
+  Subject(id: 's37', year: 'c4', title: 'Digital Marketing'),
+  Subject(id: 's38', year: 'c4', title: 'DevOps'),
+  Subject(id: 's39', year: 'c4', title: 'Machine Learning'),
+  Subject(id: 's40', year: 'c4', title: 'Deep Learning')
+];
