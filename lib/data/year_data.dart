8eb0716930f95577a5b89e06a73b067a2484ceed
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-11-30 21:06:00 +0530

    add year data

---
+++ lib/data/year_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pocketbook/model/year.dart';
+
+const YEAR_DATA = const [
+  Year(
+    id: 'c1',
+    title: 'FY',
+    color: Colors.redAccent,
+  ),
+  Year(
+    id: 'c2',
+    title: 'SY',
+    color: Colors.purple,
+  ),
+  Year(
+    id: 'c3',
+    title: 'TY',
+    color: Colors.amber,
+  ),
+  Year(
+    id: 'c4',
+    title: 'LY',
+    color: Colors.blue,
+  ),
+];
