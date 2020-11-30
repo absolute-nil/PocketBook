da199aa96451fb74a955c15ace1e4844d77eb8f0
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-11-30 21:13:05 +0530

    create app drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pocketbook/screens/book_form_screen.dart';
+import 'package:pocketbook/screens/favourite_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text("Hey there check it out!"),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text("Books"),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed("/");
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text("Favourites"),
+            onTap: () {
+              //Navigator.of(context).pushReplacementNamed(OrdersScreen.route_name);
+              Navigator.of(context).pushNamed(FavouriteScreen.id);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text("Add Book"),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(BookFormScreen.id);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Logout"),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pushReplacementNamed("/");
+              Provider.of<Auth>(context, listen: false).logout();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
