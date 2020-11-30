7d6ba710b28bf701796f3e0dd23c2d644a02570c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-11-30 21:15:21 +0530

    comment out profile screen: to be implemented with providers

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,52 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// class ProflieScreen extends StatefulWidget {
+//   static const String id = 'profile_screen';
 
-final _firestore = Firestore.instance;
-FirebaseUser loggedInUser;
+//   @override
+//   _ProflieScreenState createState() => _ProflieScreenState();
+// }
 
-class ProflieScreen extends StatefulWidget {
-  static const String id = 'profile_screen';
+// class _ProflieScreenState extends State<ProflieScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-  @override
-  _ProflieScreenState createState() => _ProflieScreenState();
-}
+//     getCurrentUser();
+//   }
 
-class _ProflieScreenState extends State<ProflieScreen> {
-  final _auth = FirebaseAuth.instance;
-  @override
-  void initState() {
-    super.initState();
+//   void getCurrentUser() async {
+//     try {
+//       final user =
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
 
-    getCurrentUser();
-  }
-
-  void getCurrentUser() async {
-    try {
-      final user = _auth.currentUser;
-      if (user != null) {
-        loggedInUser = user;
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
-              }),
-        ],
-        title: Text('PocketBook'),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-    );
-  }
-}
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: null,
+//         actions: <Widget>[
+//           IconButton(
+//               icon: Icon(Icons.close),
+//               onPressed: () {
+//                 Navigator.pop(context);
+//               }),
+//         ],
+//         title: Text('PocketBook'),
+//         backgroundColor: Colors.lightBlueAccent,
+//       ),
+//     );
+//   }
+// }
