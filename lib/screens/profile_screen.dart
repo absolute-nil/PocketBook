576e315f6942c40f239cc084a68ef76048934f8c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-12-03 20:22:08 +0530

    add profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,41 +1,406 @@
-// import 'package:flutter/material.dart';
-// class ProflieScreen extends StatefulWidget {
-//   static const String id = 'profile_screen';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pocketbook/providers/profile.dart';
+import 'package:pocketbook/widgets/app_drawer.dart';
+import 'package:provider/provider.dart';
 
-//   @override
-//   _ProflieScreenState createState() => _ProflieScreenState();
-// }
+class ProfileScreen extends StatefulWidget {
+  static const String id = 'profile_screen';
+  @override
+  MapScreenState createState() => MapScreenState();
+}
 
-// class _ProflieScreenState extends State<ProflieScreen> {
-//   @override
-//   void initState() {
-//     super.initState();
+class MapScreenState extends State<ProfileScreen>
+    with SingleTickerProviderStateMixin {
+  bool _status = true;
+  final FocusNode myFocusNode = FocusNode();
+  String name = "", rollNo = "", year = "", imageUrl;
+  final _form = GlobalKey<FormState>();
 
-//     getCurrentUser();
-//   }
+  @override
+  void initState() {
+    setState(() {
+      _status = true;
+    });
+    Future.delayed(Duration.zero).then((_) async {
+      Map<String, String> profile =
+          await Provider.of<Profile>(context, listen: false).getProfile();
+      // if (profile != null) {
+      //   setState(() {
+      //     this.name = profile['name'];
+      //     this.year = profile['year'];
+      //     this.rollNo = profile['rollno'];
+      //     this.imageUrl = profile['image'];
+      //   });
+      // }
+      setState(() {
+        _status = true;
+      });
+    });
 
-//   void getCurrentUser() async {
-//     try {
-//       final user =
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
+    super.initState();
+  }
 
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: null,
-//         actions: <Widget>[
-//           IconButton(
-//               icon: Icon(Icons.close),
-//               onPressed: () {
-//                 Navigator.pop(context);
-//               }),
-//         ],
-//         title: Text('PocketBook'),
-//         backgroundColor: Colors.lightBlueAccent,
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    print(this.name);
+    print(this.rollNo);
+    print(this.year);
+
+    final profileData = Provider.of<Profile>(context);
+    print(profileData.userId);
+    setState(() {});
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("Profile"),
+        ),
+        drawer: AppDrawer(),
+        body: new Container(
+          color: Colors.white,
+          child: Form(
+            key: _form,
+            child: new ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    new Container(
+                      height: 250.0,
+                      color: Colors.white,
+                      child: new Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.0),
+                            child: new Stack(fit: StackFit.loose, children: <
+                                Widget>[
+                              new Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new Container(
+                                      width: 140.0,
+                                      height: 140.0,
+                                      decoration: new BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: new DecorationImage(
+                                          image: NetworkImage(profileData
+                                                      .image !=
+                                                  null
+                                              ? profileData.image
+                                              : "https://img.favpng.com/24/15/4/computer-icons-avatar-user-profile-blog-png-favpng-9wEFLzf04C5m4ShX8e57uczCy.jpg"),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      )),
+                                ],
+                              ),
+                              Padding(
+                                  padding:
+                                      EdgeInsets.only(top: 90.0, right: 100.0),
+                                  child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      new CircleAvatar(
+                                        backgroundColor: Colors.red,
+                                        radius: 25.0,
+                                        child: new Icon(
+                                          Icons.camera_alt,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    ],
+                                  )),
+                            ]),
+                          )
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      color: Color(0xffFFFFFF),
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 25.0),
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 25.0),
+                                child: new Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        new Text(
+                                          'Personal Information',
+                                          style: TextStyle(
+                                              fontSize: 18.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                    new Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        _status
+                                            ? _getEditIcon()
+                                            : new Container(),
+                                      ],
+                                    )
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 25.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        new Text(
+                                          'Name',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 2.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Flexible(
+                                      child: new TextFormField(
+                                        initialValue: profileData.name,
+                                        enabled: !_status,
+                                        autofocus: !_status,
+                                        onSaved: (value) {
+                                          name = value;
+                                          print(name);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 25.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        new Text(
+                                          'Roll No',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 2.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Flexible(
+                                      child: new TextFormField(
+                                        initialValue: profileData.rollno,
+                                        enabled: !_status,
+                                        onSaved: (value) {
+                                          rollNo = value;
+                                          print(value);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 25.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        new Text(
+                                          'Year',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 2.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Flexible(
+                                      child: new TextFormField(
+                                        initialValue: profileData.year,
+                                        enabled: !_status,
+                                        onSaved: (value) {
+                                          year = value;
+                                          print(year);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 25.0),
+                                child: new Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    new Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        new Text(
+                                          'Image URL',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                    left: 25.0, right: 25.0, top: 2.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    Flexible(
+                                      child: TextFormField(
+                                        initialValue: profileData.image,
+                                        enabled: !_status,
+                                        onSaved: (value) {
+                                          imageUrl = value;
+                                          print(imageUrl);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            !_status ? _getActionButtons() : new Container(),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  Widget _getActionButtons() {
+    return Padding(
+      padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 45.0),
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Container(
+                  child: new RaisedButton(
+                child: new Text("Save"),
+                textColor: Colors.white,
+                color: Colors.green,
+                onPressed: () {
+                  setState(() {
+                    _status = true;
+                    _form.currentState.save();
+                    Provider.of<Profile>(context, listen: false)
+                        .addProfile(name, rollNo, year, imageUrl);
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                  });
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
+            ),
+            flex: 2,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Container(
+                  child: new RaisedButton(
+                child: new Text("Cancel"),
+                textColor: Colors.white,
+                color: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    _status = true;
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                  });
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
+            ),
+            flex: 2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getEditIcon() {
+    return new GestureDetector(
+      child: new CircleAvatar(
+        backgroundColor: Colors.red,
+        radius: 14.0,
+        child: new Icon(
+          Icons.edit,
+          color: Colors.white,
+          size: 16.0,
+        ),
+      ),
+      onTap: () {
+        setState(() {
+          _status = false;
+        });
+      },
+    );
+  }
+}
