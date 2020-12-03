c7902075194923b0f4e5d84347181fc177ef7305
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-12-03 20:22:44 +0530

    add image and fix get and set menthods

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -11,6 +11,7 @@ class Profile with ChangeNotifier {
   String name;
   String rollno;
   String year;
+  String image;
   final String authToken;
   final String userId;
   Profile(
@@ -18,20 +19,24 @@ class Profile with ChangeNotifier {
     this.userId,
   );
 
-  Future<void> addProfile(Profile profile) async {
+  Future<void> addProfile(
+      String name, String rollno, String year, String image) async {
     final url = "$firebaseUrl/profiles.json?auth=$authToken";
     try {
       final response = await http.post(url,
           body: json.encode({
-            'name': profile.name,
-            'rollno': profile.rollno,
-            'userId': profile.userId,
-            'year': profile.year
+            'name': name,
+            'rollno': rollno,
+            'userId': this.userId,
+            'year': year,
+            'image': image
           }));
 
       this.id = json.decode(response.body)['id'];
-      this.name = profile.name;
-      this.rollno = profile.rollno;
+      this.name = name;
+      this.rollno = rollno;
+      this.year = year;
+      this.image = image;
 
       notifyListeners();
     } catch (error) {
@@ -40,18 +45,39 @@ class Profile with ChangeNotifier {
     }
   }
 
-  Future<void> getProfile() async {
-    var url = '$firebaseUrl/profiles/$userId.json?auth=$authToken';
-    final response = await http.get(url);
-    final decodedResponse = json.decode(response.body) as Map<String, dynamic>;
-    if (decodedResponse == null) {
-      return;
+  Future<Map<String, String>> getProfile() async {
+    try {
+      print(userId);
+      final filter = 'orderBy="userId"&equalTo="${userId.toString()}"';
+      print(filter);
+      String url = '$firebaseUrl/profiles.json?auth=$authToken&$filter';
+      print(url);
+      final response = await http.get(url);
+      final decodedResponse =
+          json.decode(response.body) as Map<String, dynamic>;
+      if (decodedResponse == null) {
+        return null;
+      }
+
+      decodedResponse.forEach((profileId, profileData) {
+        this.name = profileData['name'];
+        this.rollno = profileData['rollno'];
+        this.year = profileData['year'];
+        this.image = profileData['image'];
+      });
+
+      var returnProfile = new Map<String, String>();
+      returnProfile['name'] = this.name;
+      returnProfile['year'] = this.year;
+      returnProfile['rollno'] = this.rollno;
+      returnProfile['image'] = this.image;
+
+      notifyListeners();
+
+      return returnProfile;
+    } catch (e) {
+      print("error in getting profile" + e.toString());
     }
-
-    this.name = decodedResponse['name'];
-    this.rollno = decodedResponse['rollno'];
-    this.year = decodedResponse['year'];
-
-    notifyListeners();
+    return null;
   }
 }
